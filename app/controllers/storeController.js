69c12ad47a2906168ee33238b226c77fd1b91d59
Brigham Hausman <brig@bsoahc.org>
2023-09-04 20:21:39 -0600

    send new store data to controller

--- app/controllers/storeController.js
+++ app/controllers/storeController.js
@@ -3,3 +3,11 @@ exports.homePage = (req, res) => {
     title: 'welcome home!',
   });
 };
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {title: 'Add Store'});
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,9 @@ const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
 
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore)
+
 router.get('/puggz', (req, res) => {
  res.render('pugly', {
 	title: 'food rocks!',
