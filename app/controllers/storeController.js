c60ad0fa0fab08cdca468e7ebc89c2bb2f38b85a
Brigham Hausman <brig@bsoahc.org>
2023-09-10 16:22:48 -0600

    adds save support

--- app/controllers/storeController.js
+++ app/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index', {
     title: 'welcome home!',
@@ -8,6 +11,10 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  // res.json(req.body);
+  const store = new Store(req.body);
+  await store.save(); 
+  console.log(`store: ${req.body.name} saved`);
+  res.redirect('/');
 };
